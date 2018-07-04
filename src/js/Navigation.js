e5d73ac700215b4263d1a8efe2c6de9d4dc2a9aa
Ayman Mohatarem <aymanmohatarem@gmail.com>
2018-07-04 18:57:43 -0300

    mistake update

--- src/js/Navigation.js
+++ src/js/Navigation.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './../css/navigation.css';
 import {Link} from 'react-router-dom';
-import {Nav, NavItem} from 'react-bootstrap';
+import {Nav, NavItem, Navbar} from 'react-bootstrap';
 import logo from './../images/logo.png';
 
 
@@ -9,26 +9,31 @@ import logo from './../images/logo.png';
 export default class Navigation extends Component {
     render() {
         return (
-            <div className="Navi">
-                <Link to="/"><img src={logo} className="Logo" alt="logo" /></Link>
-                <Nav bsStyle="pills" className="Navi">
-                    <NavItem eventKey={1} href="/" className="NavItem">
-                        Home
-                    </NavItem>
-                    <NavItem eventKey={1} href="/">
-                        Sign in
-                    </NavItem>
-                    <NavItem eventKey={1} href="/">
-                        Sign Up
-                    </NavItem>
-                    <NavItem eventKey={2} href="/about">
-                        About
-                    </NavItem>
-                    <NavItem eventKey={3} href="/Profile">
-                        Profile
-                    </NavItem>
-                </Nav>
-            </div>
+            <Navbar className="navigate" collapseOnSelect>
+                <Navbar.Header>
+                        <Link to="/"><img src={logo} className="Logo" alt="logo" /></Link>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                    <Nav bsStyle="pills" className="Navi" pullRight>
+                        <NavItem eventKey={1} href="/" className="NavItem">
+                            Home
+                        </NavItem>
+                        <NavItem eventKey={1} href="/">
+                            Sign in
+                        </NavItem>
+                        <NavItem eventKey={1} href="/">
+                            Sign Up
+                        </NavItem>
+                        <NavItem eventKey={2} href="/about">
+                            About
+                        </NavItem>
+                        <NavItem eventKey={3} href="/Profile">
+                            Profile
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         );
     }
 }
